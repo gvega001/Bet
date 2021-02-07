9ab409ae38de19affceb5289d6be62e3cf1cae2e
gvega001 <gvega001@gmail.com>
2021-02-06 20:34:38 -0800

    Make sure game is an interface.

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bet.Models
+﻿namespace Bet.Models
 {
-    interface Game
+    public interface Game
     {
-
+        
     }
 }
