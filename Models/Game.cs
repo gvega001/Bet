0da1ead027ba134e033adb6ad008045379bdbb52
gvega001 <gvega001@gmail.com>
2021-02-07 11:50:03 -0800

    created game interface.

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,7 +1,24 @@
-﻿namespace Bet.Models
+﻿using System.Dynamic;
+
+namespace Bet.Models
 {
     public interface Game
     {
-        
+        int GetScore();
+        string GetEventName();
+        string SetEventName();
+        int GetGuess();
+        int SetGuess();
+        int GetSmallestPossibleGuess();
+        int SetSmallestPossibleGuess();
+        int GetBiggestPossibleGuess();
+        int SetBiggestPossibleGuess();
+        BetImpl Bet();
+        void Reset();
+        bool IsGameLocked();
+        bool IsValidGuess();
+        bool IsBetWon();
+        bool IsBetLost();
+
     }
 }
