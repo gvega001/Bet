75293817495c5f4e46050730819822749ae5b8c8
gvega001 <gvega001@gmail.com>
2021-02-06 19:42:49 -0800

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bet.Startup))]
+namespace Bet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
