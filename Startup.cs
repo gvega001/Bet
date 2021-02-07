8830fa1fd0dda0973365c6662a1f13668a33ede0
gvega001 <gvega001@gmail.com>
2021-02-07 11:35:22 -0800

    Created all classes to implement interfaces. Still need all implementation.

--- Startup.cs
+++ Startup.cs
@@ -3,12 +3,14 @@ using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Bet.Startup))]
 namespace Bet
+
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
         }
     }
 }
