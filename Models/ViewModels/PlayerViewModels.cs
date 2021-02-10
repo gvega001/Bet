f9dde3157feb6779aa1fdd395952c3f364f5b786
gvega001 <gvega001@gmail.com>
2021-02-10 14:00:24 -0800

    bet details added to player view model

--- Models/ViewModels/PlayerViewModels.cs
+++ Models/ViewModels/PlayerViewModels.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bet.Models.ViewModels
 {
     public class PlayerViewModels
     {
         public Player Player { get; set; }
+        public BetImpl Bet { get; set; }
   
+
+    }
+
+    public class BetDetails
+    {
+        public LinkedList<BetImpl> Bets { get; set; }
     }
 
 }
