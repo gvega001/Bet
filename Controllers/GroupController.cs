0c828977a136ec589d7eeb7935bcf18eafa34b27
gvega001 <gvega001@gmail.com>
2021-02-10 19:26:13 -0800

    group controller created.

---
+++ Controllers/GroupController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Bet.Controllers
+{
+    public class GroupController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/ViewModels/GroupViewModels.cs
+++ Models/ViewModels/GroupViewModels.cs
@@ -1,7 +1,11 @@
-﻿namespace Bet.Models.ViewModels
+﻿using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace Bet.Models.ViewModels
 {
     public class GroupViewModels
     {
         public Group Group { get; set; }
+        public LinkedList<BetImpl> Bets { get; set; }
     }
 }
