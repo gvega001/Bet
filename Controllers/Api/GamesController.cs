7b34aaa9d92db6e9dcd333d50bc9c3134cb6b00b
gvega001 <gvega001@gmail.com>
2021-02-14 11:09:29 -0800

    Api controller for game created.

---
+++ Controllers/Api/GamesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Bet.Controllers.Api
+{
+    public class GamesController : ApiController
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
