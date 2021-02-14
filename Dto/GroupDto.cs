6b75b63810c84a1de642f11653e73dd045829e28
gvega001 <gvega001@gmail.com>
2021-02-14 14:52:18 -0800

    Group data transfter object created and impl

---
+++ Dto/GroupDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Bet.Models;
+
+namespace Bet.DTO
+{
+    public class GroupDto
+    {
+        public int Id { get; set; }
+        public int? BetId { get; set; }
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Group Name")]
+        public string GroupName { get; set; }
+        public int? PlayerId { get; set; }
+        [Required]
+        public int JoinCode { get; set; }
+
+        public LinkedList<BetImpl> Bets { get; set; }
+
+        public int? GameId { get; set; }
+    }
+}
