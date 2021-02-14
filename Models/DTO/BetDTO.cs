d8878bbc13f3380800606364d1c3b300bb02e2c7
gvega001 <gvega001@gmail.com>
2021-02-14 11:07:54 -0800

    Bet Data Transfter class created and DTO directory created.

---
+++ Models/DTO/BetDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+
+namespace Bet.Models.DTO
+{
+    public class BetDTO
+    {
+   
+        [Required]
+        public int PlayerId { get; set; }
+        [Required]
+        public Group Group { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        [Display(Name = "Amount: ")]
+        public SqlMoney? MoneyBet { get; set; }
+        [Range(0, 100000.0)]
+        public double? MaxPossibleNumber { get; set; }
+        [Range(0, 1)]
+        public double? LowestPossibleNumber { get; set; }
+
+        public double Guess { get; set; }
+        public int? GameId { get; set; }
+    }
+}
