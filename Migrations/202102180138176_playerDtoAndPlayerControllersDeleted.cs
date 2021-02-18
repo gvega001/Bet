f4a794f77ac61270a58cb56df38c0653953147ab
Gabriel Vega <gvega001@gmail.com>
2021-02-17 17:39:08 -0800

    Added migration after update

---
+++ Migrations/202102180138176_playerDtoAndPlayerControllersDeleted.cs
@@ -0,0 +1,16 @@
+namespace Bet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class playerDtoAndPlayerControllersDeleted : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
