dae05c07782f0d4425a030b6620c9eb293da281a
Gabriel Vega <gvega001@gmail.com>
2021-02-19 20:14:50 -0800

    upated view model for register page for required date of birth

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -13,7 +13,7 @@ namespace Bet.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-    
+        [Required]
         public DateTime DateOfBirth { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
